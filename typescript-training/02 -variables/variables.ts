43086a6798757e27889365e49481020ee3c4a527
abcdexter <anubhav.balodhi@gmail.com>
2024-02-04 22:51:36 +0530

    added loops

---
+++ typescript-training/02 -variables/variables.ts
@@ -0,0 +1,7 @@
+
+let firstName: string = "Anubhav";
+let lastName: string = "Balo";
+let age: number = 30;
+let height:number = 175.5;
+
+console.log(`Howdy! I'm ${firstName} + ${lastName}. I'm ${age} years old and ${height}cms tall `);
