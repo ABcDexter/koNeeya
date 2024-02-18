291218e2a9c878791d841c43f3b8296593fc94c3
abcdexter <abc.de.gen.x@gmail.com>
2024-02-18 18:48:03 +0530

    populated homepage with a table using ngFor

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
+
 export class SalesPersonListComponent implements OnInit {
 
-  constructor() { }
+  //constructor() { }
 
+  salesPersonList: SalesPerson[] = [
+	new SalesPerson("A", "B", "ab@gmail.com", 10000),
+	new SalesPerson("C", "D", "cd@gmail.com", 20000),
+	new SalesPerson("E", "F", "ef@gmail.com", 30000),
+	new SalesPerson("G", "H", "gijoe@gmail.com", 40000)
+  ]
+	
   ngOnInit(): void {
   }
 
--- angular-training/sales-project/src/app/sales-person-list/sales-person.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person.ts
@@ -1,3 +1,8 @@
 export class SalesPerson {
 	//generated class using ng generate class ClassName
+	constructor(    public firstName: string,
+			public lastName: string,
+			public email: string,
+			public salesVolume: number){
+	}
 }
