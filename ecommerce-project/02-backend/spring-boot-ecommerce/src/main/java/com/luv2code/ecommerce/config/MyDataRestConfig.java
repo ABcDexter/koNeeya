5011f09babd0582ae8cebc8c3f76809dbf72a6c4
AbcDexter <abc.de.gen.x@gmail.com>
2024-02-23 15:45:13 +0530

    added config class

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsuppoeredActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+
+        //disable HTTP methods for Product : PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuppoeredActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuppoeredActions));
+
+        //disable HTTP methods for ProductCategory : PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuppoeredActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuppoeredActions));
+    }
+}
