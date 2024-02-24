84cc7f01b01040f05e6962ed53584950b46cd9b6
AbcDexter <abc.de.gen.x@gmail.com>
2024-02-24 17:38:18 +0530

    integrated the Spring Boot service with Angular component

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) // Inject our ProductService here
+  {
+  } 
+
+  // once an Angular component is initialised it calls the ngOnInit method
   ngOnInit(): void {
+    this.listProducts();
+  }
+  listProducts() { // method is invoked once you "subscruibe"
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data; // assing results to the products array
+      }
+    )
+    //throw new Error('Method not implemented.');
   }
 
 }
